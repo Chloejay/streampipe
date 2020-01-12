3cc1c63650cd1362de195944b6c6ab95700d4448
chloejay <ji.jie@edhec.com>
2020-01-12 17:15:42 +0800

    update spark streaming test

--- kafka/kafkatest/kafka_spark_df.py
+++ kafka/kafkatest/kafka_spark_df.py
@@ -1,5 +1,5 @@
 #!streampipe/kafka/kafkatest 
-
+from __future__ import print_function
 import sys
 from pyspark import SparkContext
 from pyspark import SparkConf
@@ -9,43 +9,59 @@ from pyspark.sql import Row, DataFrame, SQLContext
 from pyspark.sql import SparkSession 
 
 
-def process(time, rdd):
-    print("========= %s =========" % str(time))
+# https://github.com/apache/spark/blob/v2.2.1/examples/src/main/python/streaming/direct_kafka_wordcount.py#L48
+def process(rdd):
     try:
-        sqlContext = getSqlContextInstance(rdd.context)
-        rowRdd = rdd.map(lambda w: Row(word=w[0], cnt=w[1]))
-        wordsDataFrame = sqlContext.createDataFrame(rowRdd)
-        wordsDataFrame.show()
-
-        wordsDataFrame.createOrReplaceTempView("words")
-        wordCountsDataFrame = spark.sql("select SUM(cnt) as total from words")
-        wordCountsDataFrame.show()
+        rowRdd = rdd.map(lambda w: Row(color=w[0],num=w[1]))
+        df = sqlContext.createDataFrame(rowRdd)
+        df.show(n=5) 
+        df.createOrReplaceTempView("count") 
+        df_count= sqlContext.sql("select SUM(num) as total from count")
+        df_count.show() 
 
     except Exception as e:
-        print('error is'.format(e)) 
+        print('error is {}'.format(e)) 
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print("Usage: kafka_spark_df.py <zk> <topic>", file=sys.stderr)
         exit(-1)
 
-
-export JAVA_HOME=/Users/chloeji/jdk-11.0.5.jdk/Contents/Home/
-
-
-
     sc = SparkContext.getOrCreate() 
     sqlContext = SQLContext(sc)
-    ssc = StreamingContext(sc, 100)
-
+    ssc = StreamingContext(sc, 10)
     zkQuorum, topic = sys.argv[1:]
     kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
+
     lines = kvs.map(lambda x: x[1])
     words = lines.flatMap(lambda line: line.split(" ")) \
-        .map(lambda word: (word, 1)) \
+        .map(lambda x: (x, 1)) \
         .reduceByKey(lambda a, b: a+b)
+    words.pprint() 
 
     words.foreachRDD(process)
-
     ssc.start()
-    ssc.awaitTermination()
+    ssc.awaitTermination() 
+
+# https://github.com/apache/spark/blob/master/docs/structured-streaming-kafka-integration.md
+# create kafka source 
+# spark = SparkSession \
+#     .builder \
+#     .appName("test") \
+#     .getOrCreate()
+
+# def read_kafka(topic):
+#     df = (spark.read
+#     .format("kafka") 
+#     .option("kafka.bootstrap.servers", "localhost:9092") 
+#     .option("subscribe",topic)
+#     .option("startingOffsets", "earliest")
+#     .option("endingOffsets", "latest") 
+#     .load()) 
+#     # df.writeStream.format("text").option("checkpointLocation", "test.txt").option("path","test2.txt").start()
+#     # df.writeStream.outputMode("append").format("console").start()
+#     # df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
+#     return df
+
+# read_kafka('kafkatesting') 
